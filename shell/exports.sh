3f7a582eaef1ea309bbd205bf9e87d729c035878
dominicparga <dominic.parga@gmail.com>
2019-03-19 19:09:23 +0100

    Add exports.sh to shellrc

---
+++ shell/exports.sh
@@ -0,0 +1,33 @@
+################################################################################
+# coreutils
+
+if ( is_machine 'macOS' ); then
+    # head links to ghead instead of macOS-head
+    PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+fi
+
+################################################################################
+# general
+
+export EDITOR='vi'
+export VISUAL='code'
+
+################################################################################
+# python
+
+if ( is_machine 'macOS' ); then
+    # used in vscode to find a default python interpreter
+    export PYTHON_INTERPRETER_PATH='/usr/local/bin/python3'
+
+    export PATH="${HOME}/Library/Python/2.7/bin:${PATH}"
+    export PATH="${HOME}/Library/Python/3.7/bin:${PATH}"
+fi
+
+################################################################################
+# java
+
+if ( is_machine 'macOS' ); then
+    export JAVA_HOME="$(/usr/libexec/java_home)"
+    export PATH="${JAVA_HOME}:${PATH}"
+    export PATH="${JAVA_HOME}/bin:${PATH}"
+fi
