4d7e3c2dbb2c26099a2fc4f2249e53e02918682c
dominicparga <dominic.parga@gmail.com>
2019-03-22 19:40:53 +0100

    adds install script for brew formulae

--- install/macOS/brew_formulae.sh
+++ install/macOS/brew_formulae.sh
@@ -1,1 +1,50 @@
-echo "TODO: install brew formulae"
+################################################################################
+# update brew first
+
+if ( which brew 1>/dev/null 2>/dev/null ); then
+    echo "Installing homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Updating homebrew and formulae..."
+    brew update
+    brew upgrade
+    brew cask upgrade
+    brew cleanup
+fi
+
+################################################################################
+# define and install formulae
+
+# important order
+brew cask install java
+brew install scala
+brew install sbt
+
+# unordinary formulae in alphabetical order
+brew cask install alfred
+brew install coreutils
+brew cask install discord
+brew cask install dropbox
+brew cask install enpass
+brew cask install firefox
+brew cask install gimp
+brew install git
+brew install grep
+brew install htop
+brew cask install microsoft-office
+brew cask install musescore
+brew install neovim
+brew install python
+brew install python@2
+brew cask install spotify
+brew cask install teamspeak-client
+brew cask install Telegram
+brew install tree
+brew cask install visual-studio-code
+brew cask install vlc
+brew install zsh
+brew install heroku/brew/heroku
+
+# needs extra stuff
+brew cask install virtualbox
+brew cask install teamviewer # needs password
