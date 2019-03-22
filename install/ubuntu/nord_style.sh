e4dd0a30bc99153c3ba17c3325488fbadd4730dd
dominicparga <dominic.parga@gmail.com>
2019-03-22 19:43:35 +0100

    adds install script (ubuntu) for Nord style

--- install/ubuntu/nord_style.sh
+++ install/ubuntu/nord_style.sh
@@ -1,1 +1,6 @@
-echo "TODO: install nord style"
+################################################################################
+# install nord style for terminal
+
+mkdir -p "${DOTFILES}/custom/ubuntu"
+curl -so "${DOTFILES}/custom/ubuntu/nord.sh" https://raw.githubusercontent.com/arcticicestudio/nord-gnome-terminal/develop/src/nord.sh
+sh "${DOTFILES}/custom/ubuntu/nord.sh"
