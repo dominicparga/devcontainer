4b3a2c76266e001c74bb8674aa2e8b4716c3e35a
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2019-11-17 22:11:24 +0100

    Improve script for creating gitignore-file

--- utils/create_gitignore.sh
+++ utils/create_gitignore.sh
@@ -1,34 +1,12 @@
 #!/usr/bin/env sh
 
-#------------------------------------------------------------------------------#
-# download output from gitignore and append it to content
-
-__CONTENT="\
-#------------------------------------------------------------------------------#
-# gitignore\
-"
-
-__IGNORED_ITEMS="macos,visualstudiocode"
-__CONTENT="${__CONTENT}$(curl -L -s https://www.gitignore.io/api/${__IGNORED_ITEMS})\n"
-
-
-
-#------------------------------------------------------------------------------#
-# append custom
-
-__CONTENT="${__CONTENT}\n\
-\n\
-\n\
-#------------------------------------------------------------------------------#
-# Custom
-
-/custom/
-
-.vscode/\n"
-
-
-
-#------------------------------------------------------------------------------#
-# print to console for piping
-
-printf "${__CONTENT}"
+echo '#------------------------------------------------------------------------------#'
+echo '# gitignore'
+curl -L -s 'https://www.gitignore.io/api/macos,visualstudiocode'
+echo ''
+echo '#------------------------------------------------------------------------------#'
+echo '# custom'
+echo ''
+echo '/custom/'
+echo ''
+echo '.vscode/'
