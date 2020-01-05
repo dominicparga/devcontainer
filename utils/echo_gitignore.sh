13332bc8e9801ec8d0c94a778dd0ec47a9f6efbc
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-01-05 12:34:25 +0100

    Rename gitignore-script and edit .gitignore

--- utils/echo_gitignore.sh
+++ utils/echo_gitignore.sh
@@ -2,11 +2,12 @@
 
 echo '#------------------------------------------------------------------------------#'
 echo '# gitignore'
-curl -L -s 'https://www.gitignore.io/api/macos,visualstudiocode'
+curl -L -s 'https://www.gitignore.io/api/code,linux,macos,visualstudiocode,windows'
 echo ''
 echo '#------------------------------------------------------------------------------#'
 echo '# custom'
 echo ''
 echo '/custom/'
+echo 'build/'
 echo ''
 echo '.vscode/'
