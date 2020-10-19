312033e724e34728489a6660a220e00236fa82d0
Fabian Franzelin <fabian.franzelin@de.bosch.com>
2020-10-19 10:52:34 +0200

    update howto

--- emacs/.emacs.d/howto_build_emacs.sh
+++ emacs/.emacs.d/howto_build_emacs.sh
@@ -11,23 +11,24 @@ sudo apt-get install -y \
      libncurses5-dev \
      libjansson-dev
 
-
 # clone and configure repo
-VERSION=27.1
-SRC_PATH=$HOME/opt/emacs-$VERSION
-git clone git://git.sv.gnu.org/emacs.git $SRC_PATH
-cd emacs
+VERSION="27.1"
+SRC_PATH="${HOME}/opt/emacs-${VERSION}"
+# git clone git://git.sv.gnu.org/emacs.git "${SRC_PATH}"
+cd "${SRC_PATH}" || exit
 ./autogen.sh
-CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" ./configure --with-modules --with-json --prefix=$HOME/opt/emacs-$VERSION --bindir=$HOME/opt/emacs-$VERSION/bin
+CFLAGS="-ggdb3 -O0" CXXFLAGS="-ggdb3 -O0" LDFLAGS="-ggdb3" ./configure --with-modules --with-json --prefix="${HOME}/opt/emacs-${VERSION}" --bindir="${HOME}/opt/emacs-${VERSION}/bin"
 
 # Build and install
-make -j$(nproc)
+make -j"$(nproc)"
 make install
 
 # install alternatives for emacs and emacsclient
-sudo update-alternatives --install /usr/bin/emacs emacs $SRC_PATH/bin/emac 1
-sudo update-alternatives --install /usr/bin/emacsclient emacsclient $SRC_PATH/bin/emacsclient 1
+sudo update-alternatives --install /usr/bin/emacs emacs "${SRC_PATH}/bin/emacs" 1
+sudo update-alternatives --install /usr/bin/emacsclient emacsclient "${SRC_PATH}/bin/emacsclient" 1
 
 # update alternatives to new version
 sudo update-alternatives --config emacs
 sudo update-alternatives --config emacsclient
+
+cd - || exit
