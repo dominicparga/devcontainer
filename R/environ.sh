24c08fc53d3ab028baa5ee03e124c81d4a1ef403
Parga Cacheiro, Dominic <dominic.parga@gmail.com>
2020-10-27 15:08:10 +0100

    Cleanup R-setup

--- R/environ.sh
+++ R/environ.sh
@@ -6,7 +6,10 @@
 # libPaths
 # https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/libPaths
 
+# only VAR=VALUE per line, nothing else!
+
 #R_HOME="${HOME}/R"
 R_BROWSER="brave"
-R_LIBS_USER="${HOME}/R/library/4.0" # only in .libPaths() if dir exists
+# only in .libPaths() if dir exists
+R_LIBS_USER="${HOME}/R/library/4.0"
 R_HISTFILE="${HOME}/.Rhistory"
--- R/profile.R
+++ R/profile.R
@@ -2,14 +2,24 @@
 
 # The .First function is called after everything else in .Rprofile is executed
 .First <- function() {
-  # Print a welcome message
-  message("Welcome back ", Sys.getenv("USER"),"!")
-  message("- working directory is ", getwd())
-  collapse_str = "\n  - "
-  message("- .libPaths() is", collapse_str, paste(.libPaths(), collapse = collapse_str))
+    # Print a welcome message
+    message("Welcome back ", Sys.getenv("USER"), "!")
+    message("- working directory is ", getwd())
+    collapse_str <- "\n  - "
+    message(
+        "- .libPaths() is",
+        collapse_str,
+        paste(.libPaths(), collapse = collapse_str)
+    )
 }
 
-options(digits = 12)                                          # number of digits to print. Default is 7, max is 15
-options(stringsAsFactors = FALSE)                             # Disable default conversion of character strings to factors
-options(show.signif.stars = FALSE)                            # Don't show stars indicating statistical significance in model outputs
-error <- quote(dump.frames("${R_HOME_USER}/testdump", TRUE))  # post-mortem debugging facilities
+# number of digits to print. Default is 7, max is 15
+options(digits = 12)
+# Disable default conversion of character strings to factors
+options(stringsAsFactors = FALSE)
+# Don't show stars indicating statistical significance in model outputs
+options(show.signif.stars = FALSE)
+# show all warnings and show them when happening
+options(warn = 1)
+# post-mortem debugging facilities
+error <- quote(dump.frames("${R_HOME}/testdump", TRUE))
