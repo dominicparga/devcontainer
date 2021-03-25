76e51060855da1ec4aa920f503093e6276d1fab8
Fabian Franzelin (XC-AD/ESB4) <fabian.franzelin@de.bosch.com>
2021-03-25 09:19:53 +0100

    enable shortcuts for buffer move mode

--- emacs/init.el
+++ emacs/init.el
@@ -220,12 +220,10 @@
 ;; Buffer move
 ;; -------------------------------------------------------------------
 (use-package buffer-move
-  :config
-  (setq buffer-move-stay-after-swap t)
-  :bind (("C-S-<down>" . buffer-move-down)
-         ("C-S-<up>" . buffer-move-up)
-         ("C-S-<left>" . buffer-move-left)
-         ("C-S-<right>" . buffer-move-right))
+  :bind (("C-c b <down>" . buf-move-down)
+         ("C-c b <up>" . buf-move-up)
+         ("C-c b <left>" . buf-move-left)
+         ("C-c b <right>" . buf-move-right))
   )
 
 ;; -------------------------------------------------------------------
@@ -1531,7 +1529,25 @@
  '(package-selected-packages
    '(with-editor company-jedi buffer-move dired-hide-dotfiles dired-open all-the-icons-dired dired-single yasnippet-snippets ccls git-gutter-fringe yaml-mode xterm-color whole-line-or-region which-key wgrep vterm volatile-highlights use-package-ensure-system-package tide super-save sphinx-doc smex smart-mode-line scala-mode rainbow-delimiters pyvenv python-black py-autopep8 protobuf-mode poly-rst ox-rst openwith multi-term material-theme magit-todos lsp-ui lsp-python-ms lsp-java lsp-ivy lsp-docker langtool json-mode ivy-rich ivy-prescient ivy-pass ivy-hydra highlight-symbol groovy-mode groovy-imports git-timemachine general flymake-yaml flymake-shellcheck flymake-shell flymake-json flycheck-pycheckers flycheck-plantuml flx fill-column-indicator exec-path-from-shell evil-nerd-commenter ess eshell-z emojify edwina drag-stuff dockerfile-mode dired-narrow diminish counsel-projectile company-prescient company-c-headers company-auctex command-log-mode cmake-mode clang-format+ blacken beacon autopair auto-package-update auto-complete ag ack))
  '(safe-local-variable-values
-   '((projectile-project-test-cmd . "python -m pytest ./tests -vv")
+   '((eval progn
+           (setq flycheck-python-mypy-config
+                 '(".mypy.ini"))
+           (setq flycheck-pylintrc ".pylintrc")
+           (setq flycheck-checker 'python-pylint)
+           (set
+            (make-local-variable 'main)
+            (concat
+             (projectile-project-root)))
+           (set
+            (make-local-variable 'sphinx-extensions)
+            (concat
+             (projectile-project-root)
+             "python"))
+           (set
+            (make-local-variable 'python-path)
+            (concat main ":" sphinx-extensions))
+           (setenv "PYTHONPATH" python-path))
+     (projectile-project-test-cmd . "python -m pytest ./tests -vv")
      (projectile-project-compilation-cmd . "./build -b html -d architecture")
      (eval progn
            (setq flycheck-python-mypy-config
